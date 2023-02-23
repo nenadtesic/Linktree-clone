b5778c8598f9c7f11549988fb60e240d26ee7748
nenadtesic <nndtesic@gmail.com>
2023-02-23 02:52:29 +0100

    linktree app

---
+++ app.js
@@ -0,0 +1,15 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+
+async function copyText (e) {
+    //prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text " + link)
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButtons => shareButtons.addEventListener('click', copyText))
